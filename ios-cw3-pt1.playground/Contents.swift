0728d5b9949a724e9cf435bd496b9fa29dff59ea
C87-Amal-Sulaiman-Alfadhel <70811155+C87-Amal-Sulaiman-Alfadhel@users.noreply.github.com>
2020-09-16 20:34:29 +0300

    cw3 done

---
+++ ios-cw3-pt1.playground/Contents.swift
@@ -0,0 +1,7 @@
+
+func printMe () {
+    print ("My name is Amal.")
+    print ("I am 17 years old.")
+    print ("I love watching Formula 1!")
+}
+printMe()
---
+++ ios-cw3-pt2.playground/Contents.swift
@@ -0,0 +1,6 @@
+
+func add (number1 : Int, number2 : Int) -> Int {
+    return(number1 + number2)
+}
+var functionCall = add(number1: 5, number2: 8)
+print (functionCall)
